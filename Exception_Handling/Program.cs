fd6e6eb9a912be9f430f6988fc8d8ae3ed82fdb6
JuliaKramskoy <julia.kramskoy@gmail.com>
2020-10-06 16:17:27 -0500

    new project file

---
+++ Exception_Handling/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exception_Handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //user input
+            Console.Write("Please add first number: ");
+            int firtsNum = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please add second number: ");
+            int secondNum = Convert.ToInt32(Console.ReadLine());
+
+            //output
+            Console.WriteLine("${0}/{1}={3}",firtsNum,secondNum,firtsNum/secondNum);
+
+        }
+    }
+}
