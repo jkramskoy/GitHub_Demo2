f9ff5b1110b1e27ce4640152bcea5fe75e836b15
JuliaKramskoy <julia.kramskoy@gmail.com>
2020-10-06 21:00:20 -0500

    check validation

--- Exception_Handling/Program.cs
+++ Exception_Handling/Program.cs
@@ -6,16 +6,37 @@ namespace Exception_Handling
     {
         static void Main(string[] args)
         {
-            
-            //user input
+            bool isWrongCondition = false;
             Console.Write("Please add first number in the range 0-255: ");
             int firtsNum = Convert.ToInt32(Console.ReadLine());
             Console.Write("Please add second number in the range 0-255: ");
             int secondNum = Convert.ToInt32(Console.ReadLine());
+            //user input
+            do {
+                if (isValidNumber(firtsNum) && isValidNumber(secondNum))
+                {
+                    Console.WriteLine("{0} / {1} = {2}", firtsNum, secondNum, firtsNum / secondNum);
+                    isWrongCondition = false;
+                }
+                else
+                {
+                    Console.WriteLine("Error!!Please add number in the range 0-255");
+                    Console.Write("Please add first number in the range 0-255: ");
+                    firtsNum = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Please add second number in the range 0-255:");
+                    secondNum = Convert.ToInt32(Console.ReadLine());
+                    isWrongCondition = true;
+                }
+            } while (isWrongCondition);
+        }
 
-            //output
-            Console.WriteLine("{0} / {1} = {2}", firtsNum, secondNum, firtsNum / secondNum);
-
+        static bool isValidNumber(int num)
+        {
+            if (num >= 0 && num <= 255)
+            {
+                return true;
+            }
+            else return false;
         }
     }
 }
